ff9e52de7ceec38ce1050416fc7737b623e1245b
Jean Carlo Emer <jeancarloemer@gmail.com>
2013-11-04 22:45:42 -0200

    Add more two speakers

--- docpad.js
+++ docpad.js
@@ -68,6 +68,19 @@ module.exports = {
     // The entire schedule
     schedule: [
       {
+        name: "Jaydson Gomes",
+        gravatar: "572696200604e59baa59ee90d61f7d02",
+        bio: "Mais de 8 anos de experiência em desenvolvimento Web, apaixonado por JavaScript, curador da BrazilJS Conf e um dos criadores e líderes da BrazilJS Foundation. Trabalha atualmente no Terra criando ferramentas e APIs JavaScript e pesquisando novas tecnologias, como HTML5 e todo o seu guarda-chuva, Node.JS e, é claro, JavaScript",
+        comment: "Apoiado na evolução do desenvolvimento front-end e como um verdadeiro <a href=\"http://jaydson.org/cultura-maldita-no-desenvolvimento-de-software/\" target=\"_blank\">exterminar <del>do futuro</del> da cultura maldita,</a> Jaydson vai falar sobre a revolução a qual liderou em nome de produtos mais robustos, escaláveis e de qualidade.",
+        company: "Terra",
+        twitter: "jaydson",
+        github: "jaydson",
+        presentation: {
+          title: "Processo de Desenvolvimento FrontEnd - Do caos ao Sublime",
+          description: "O mundo muda constantemente. Na tecnologia isso é mais evidente, o avanço é exponêncial, e quase não conseguimos acompanhar o que há de mais novo na área em que atuamos. No desenvolvimento de software, a mudança pode influenciar diretamente no sucesso do mesmo. Novas tecnologias, novas ferramentas, novas linguagens, tudo isso deve ser considerado. O JavaScript ocupou o lugar de linguagem mais querida e popular no mundo, depois de passar anos sendo incompreendida. Esta mudança é reflexo do avanço da linguagem, das ferramentas e de todas as tecnologias que a cercam. O modo de se desenvolver código client-side mudou, e para melhor. O ambiente de desenvolvimento não é mais o mesmo, hoje temos um arsenal de ferramentas que nos ajudam a criar aplicações robustas, escaláveis e de qualidade. Praticamente tudo que existe para melhorar o desenvolvimento de software em alguma outra linguagem, também existe para JavaScript. Testes unitários, testes funcionais, ferramentas de build, validação de sintaxe, validação de convenções, integração contínua, etc. Todos os itens acima, e muitos outros, são essenciais em qualquer tipo de software, e atualmente temos o poder de tornar nossas aplicações JavaScript muito mais confiáveis do que há alguns anos. O objetivo da palestra é apresentar as principais ferramentas, tecnologias e técnicas disponíveis para o desenvolvimento de aplicações client-side, além de mostrar na prática, como mudamos completamente o processo de desenvolvimento FrontEnd em um dos maiores portais da América Latina, o Terra."
+        }
+      },
+      {
         name: "Vitor Carlos",
         gravatar: "8831190d849c1748dbb2bcdffbf71bc9",
         bio: "Um gamer da geração atari, *.dev, apaixonado por café e UX. Estuda Jogos Digitais na PUCRS e trabalha como Front-End na Joy Interactive.",
@@ -91,6 +104,19 @@ module.exports = {
         presentation: {
           title: "Responsive Web Workflow",
           description: "Trabalhar com Web Design Responsivo pode ser confuso, é um cenário em constante e rápida evolução, cheio de diferentes opções para escolher e se adaptar, novas tendências. Não existe bala de prata, mas nessa talk vamos avaliar práticas sugeridas e escolher nossas metodologias com um toolset básico para montar um projeto com nossas próprias guidelines."
+        }
+      },
+      {
+        name: "Randal Maia",
+        gravatar: "02bb6d790ea355609c1eaed994465054",
+        bio: "Já foi sobrinho, menino do HTML e atualmente, Front-end Engineer no ContaAzul, startup B2B brasileira. Focado em resultados, acredita na influência de um bom front-end na experiência do usuário e nas métricas de um software. Também entusiasta web, empreendedor e graduando em Análise de Sistemas pela UDESC.",
+        comment: "Viu no crescimento da equipe em que trabalha o desafio de acelerar o desenvolvimento e manter a qualidade do produto. Randal nos apresentará o processo de criação de um Guia de Estilos, solução para seus problemas.",
+        company: "ContaAzul",
+        twitter: "randalmaia",
+        github: "randalmaia",
+        presentation: {
+          title: "Acelere, faça amigos e aprenda com seu guia de estilos",
+          description: "Sabe aquele momento em que o designer entrega milhões de telas e você sente que não vai dar conta de todas as tarefas? Nesta talk, quero falar sobre como acelerar seu processo de desenvolvimento, fazer amigos dentro da sua equipe, compartilhar seu conhecimento e ter seu próprio guia de estilos."
         }
       }
     ],
