36d38a1a63164c6250af3b26f54007012c1fb7d6
Jean Carlo Emer <jeancarloemer@gmail.com>
2014-10-06 10:18:38 -0300

    Change map style

--- 2014/scripts/main.js
+++ 2014/scripts/main.js
@@ -1,17 +1,17 @@
 // Location map (async init)
 // ===========
 window.mapsAsyncInit = function () {
+  // style from http://snazzymaps.com/style/74/becomeadinosaur
+  var mapStyle = [{elementType:"labels.text",stylers:[{visibility:"off"}]},{featureType:"landscape.natural",elementType:"geometry.fill",stylers:[{color:"#f5f5f2"},{visibility:"on"}]},{featureType:"administrative",stylers:[{visibility:"off"}]},{featureType:"transit",stylers:[{visibility:"off"}]},{featureType:"poi.attraction",stylers:[{visibility:"off"}]},{featureType:"landscape.man_made",elementType:"geometry.fill",stylers:[{color:"#ffffff"},{visibility:"on"}]},{featureType:"poi.business",stylers:[{visibility:"off"}]},{featureType:"poi.medical",stylers:[{visibility:"off"}]},{featureType:"poi.place_of_worship",stylers:[{visibility:"off"}]},{featureType:"poi.school",stylers:[{visibility:"off"}]},{featureType:"poi.sports_complex",stylers:[{visibility:"off"}]},{featureType:"road.highway",elementType:"geometry",stylers:[{color:"#ffffff"},{visibility:"simplified"}]},{featureType:"road.arterial",stylers:[{visibility:"simplified"},{color:"#ffffff"}]},{featureType:"road.highway",elementType:"labels.icon",stylers:[{color:"#ffffff"},{visibility:"off"}]},{featureType:"road.highway",elementType:"labels.icon",stylers:[{visibility:"off"}]},{featureType:"road.arterial",stylers:[{color:"#ffffff"}]},{featureType:"road.local",stylers:[{color:"#ffffff"}]},{featureType:"poi.park",elementType:"labels.icon",stylers:[{visibility:"off"}]},{featureType:"poi",elementType:"labels.icon",stylers:[{visibility:"off"}]},{featureType:"water",stylers:[{color:"#71c8d4"}]},{featureType:"landscape",stylers:[{color:"#e5e8e7"}]},{featureType:"poi.park",stylers:[{color:"#8ba129"}]},{featureType:"road",stylers:[{color:"#ffffff"}]},{featureType:"poi.sports_complex",elementType:"geometry",stylers:[{color:"#c7c7c7"},{visibility:"off"}]},{featureType:"water",stylers:[{color:"#a0d3d3"}]},{featureType:"poi.park",stylers:[{color:"#91b65d"}]},{featureType:"poi.park",stylers:[{gamma:1.51}]},{featureType:"road.local",stylers:[{visibility:"off"}]},{featureType:"road.local",elementType:"geometry",stylers:[{visibility:"on"}]},{featureType:"poi.government",elementType:"geometry",stylers:[{visibility:"off"}]},{featureType:"landscape",stylers:[{visibility:"off"}]},{featureType:"road",elementType:"labels",stylers:[{visibility:"off"}]},{featureType:"road.arterial",elementType:"geometry",stylers:[{visibility:"simplified"}]},{featureType:"road.local",stylers:[{visibility:"simplified"}]},{featureType:"road"},{featureType:"road"},{},{featureType:"road.highway"}];
+
   var container = document.querySelector('[data-address]')
     , address   = container.getAttribute('data-address')
     , options   = {
-        zoom: 16
+        zoom: 15
       , scrollwheel: false
-      , mapTypeControl: false
-      , labels: true
-      , zoomControlOptions: {
-          position: google.maps.ControlPosition.TOP_RIGHT
-        }
+      , disableDefaultUI: true
       , mapTypeId: google.maps.MapTypeId.ROADMAP
+      , styles: mapStyle
     }
 
   var map    = new google.maps.Map(container, options)
