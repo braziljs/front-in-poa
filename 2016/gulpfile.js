1499b648bfcf56c72daf30f160c607f092c10a8d
Felipe N. Moura <felipenmoura@gmail.com>
2016-09-26 16:48:18 -0300

    Atualizando a versão

---
+++ 2016/gulpfile.js
@@ -0,0 +1,42 @@
+var gulp = require('gulp');
+var rename = require('gulp-rename');
+
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+
+var plumber = require('gulp-plumber');
+var webserver = require('gulp-webserver');
+
+
+gulp.task('styles', function () {
+  gulp.src('./styles/main.scss')
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(autoprefixer())
+    .pipe(rename('./main.css'))
+    .pipe(gulp.dest('./styles/'));
+});
+
+/*
+ * Default task and server/watch
+*/
+
+gulp.task('webserver', function() {
+  gulp.src('./')
+    .pipe(webserver({
+      host: '0.0.0.0',
+      directoryListing: true,
+      livereload: false
+    }));
+});
+
+gulp.task('watch', function(){
+  gulp.watch('./styles/**/*.scss', ['styles']);
+});
+
+gulp.task('default', [
+  'styles',
+
+  'webserver',
+  'watch'
+]);
