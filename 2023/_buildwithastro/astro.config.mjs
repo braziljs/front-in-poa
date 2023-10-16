2c8d6b506d271fad64e30bab84c78b4f6d8658b5
ROBSON.JUNIOR <robson.ga.junior@gmail.com>
2023-10-16 19:25:53 -0300

    fix: components styles

--- 2023/_buildwithastro/astro.config.mjs
+++ 2023/_buildwithastro/astro.config.mjs
@@ -1,4 +1,23 @@
 import { defineConfig } from 'astro/config';
+const productionBuild = import.meta.env.PROD;
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://www.frontinpoa.com.br/',
+    compressHTML: true,
+    // trailingSlash: 'always', // for server
+    output: 'static',
+    vite: {
+        ssrBuild: true,
+        server: {
+            // https://stackoverflow.com/questions/41553291/can-you-import-nodes-path-module-using-import-path-from-path
+            fs: {
+                // Allow serving files from one level up to the project root
+                allow: ['..'],
+            }
+        },
+        ssr: {
+            noExternal: productionBuild ? true : [],
+            external: [],
+        }
+    }
+});
